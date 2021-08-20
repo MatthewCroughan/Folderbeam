b8dea0a7ceb4dcc7aacb6cbdec662b7c3989bc0f
solvencino <harshparamar84@gmail.com>
2021-08-20 19:58:00 +0530

    add commander

--- bin/index.js
+++ bin/index.js
@@ -1,46 +1,107 @@
 #!/usr/bin/env node
 
+import { Command } from "commander";
+import rw from "random-words";
 import Hyperbeam from "hyperbeam";
 import { createTar, extractTar } from "../lib/main.mjs";
-import rw from "random-words";
-import minimist from "minimist";
 
-const args = minimist(process.argv.slice(2));
-let key, beam;
+const program = new Command();
 
-if (args.i && args.o) {
+program
+  .option("-i, --indir <name>", "name of input folder")
+  .option("-o, --outdir <name>", "name of output folder")
+  .option("-k, --key <words>", "2-3 words long key")
+  .addHelpText(
+    "after",
+    `
+
+  Example usage :
+
+  on alice's machine : folderbeam -i <foldername> -k 'random words'
+  on bob's machine : folderbeam -o <foldername> -k 'same random words'
+
+  on source machine random words will be generated if not passed
+  `
+  )
+  .parse();
+
+const opts = program.opts();
+
+if (Object.keys(opts).length === 0) {
+  program.help();
+}
+if (opts.indir && opts.outdir) {
   console.log(
-    "Only use either input[-i] or output[-o], 'folderbeam help' for usage "
+    "error: only use one input[-i] or output[-o] at a time, folderbeam --help for usage"
   );
+  process.exit();
 }
 
-key = args.w;
+if (opts.indir) {
+  let key = opts.key;
+  if (!opts.key) {
+    key = rw({ exactly: 3, join: " " });
+  }
 
-if (!args.w && args.o) {
-  console.log("Key is required!");
-} else if (!args.w) {
-  key = rw({ exactly: 3, join: " " });
-}
+  const beam = new Hyperbeam(key);
+  const stream = createTar(opts.indir);
+  setupBeam(beam);
 
-if (args.i) {
-  beam = new Hyperbeam(key);
-  console.log(`\n\t\t ${key} \t\t\n`);
+  console.log(`\n\t\t KEY : '${key}' \t\t\n`);
 
-  const stream = createTar(args.i);
   stream.on("end", () => {
-    console.log("Folder Beaming Complete!");
+    console.log("Folder Beaming Completed!");
     beam.end();
   });
+
   stream.pipe(beam);
-} else {
-  beam = new Hyperbeam(key);
+} else if (opts.outdir) {
+  if (!opts.key) {
+    console.log("error: key required!, folderbeam --help for usage");
+    process.exit();
+  }
+  const beam = new Hyperbeam(opts.key);
 
-  const stream = extractTar(args.o);
+  const stream = extractTar(opts.outdir);
 
+  setupBeam(beam);
   beam.pipe(stream);
 
   beam.on("end", () => {
-    console.log("Folder Beaming Complete!");
+    console.log("Folder Beaming Completed!");
     beam.end();
+  });
+}
+
+function setupBeam(beam) {
+  beam.on("remote-address", function ({ host, port }) {
+    if (!host) console.error("[hyperbeam] Could not detect remote address");
+    else
+      console.error(
+        "[hyperbeam] Joined the DHT - remote address is " + host + ":" + port
+      );
+    if (port) console.error("[hyperbeam] Network is holepunchable \\o/");
+  });
+
+  beam.on("connected", function () {
+    console.error(
+      "[hyperbeam] Success! Encrypted tunnel established to remote peer"
+    );
+    console.log("Beaming Folder...");
+  });
+
+  process.once("SIGINT", () => {
+    if (!beam.connected) closeASAP(beam);
+    else beam.end();
+  });
+}
+
+function closeASAP(beam) {
+  console.error("[hyperbeam] Shutting down beam...");
+
+  const timeout = setTimeout(() => process.exit(1), 2000);
+  beam.destroy();
+  beam.on("close", function () {
+    clearTimeout(timeout);
   });
 }
